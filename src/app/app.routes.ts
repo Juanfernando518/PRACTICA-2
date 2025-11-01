bb149771ec875aae99898f0edb78ff5352281009
Juanfernando518 <jfernandoalz18@gmail.com>
2025-11-01 16:27:03 -0500

    CAMBIOS

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,18 @@ import { Routes } from '@angular/router';
 import { HomePage } from './features/homePage/homePage';
 import { PerfilPage } from './features/PerfilPage/PerfilPage';
 
-
 export const routes: Routes = [
-    {path:"",
-        component: HomePage
-
-    },
-    {
-        path: "perfil",
-        component: PerfilPage
-    }
+  {
+    path: '',
+    redirectTo: 'perfil',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomePage
+  },
+  {
+    path: 'perfil',
+    component: PerfilPage
+  }
 ];
